f1c6dd9a18bf96098c5ae4366d0c98afc6b9f10d
MikhailBarakovskiy <89397907+MikhailBarakovskiy@users.noreply.github.com>
2021-10-22 11:22:23 +0300

    Update query_killer.py
    refract some functions

--- query_killer.py
+++ query_killer.py
@@ -1,48 +1,50 @@
 import difflib
 
+usernames = "'TEAMCITY', 'POWERBI', 'ETL', 'ETL_EMERGENCY'"
 
-def similar_queries(connect, match_percentage=0.8):
-    query_list = connect.query('dwh', """SELECT QUERY_ID, QUERY_TEXT FROM TABLE(UTIL_DB.information_schema.query_history(RESULT_LIMIT => 100)) 
-        WHERE EXECUTION_STATUS = 'RUNNING' AND USER_NAME NOT IN ('TEAMCITY', 'POWERBI', 'ETL', 'ETL_EMERGENCY')""")
+
+def get_similar_queries(connect, match_percentage=0.8, names=usernames):
+    query_list = connect.query('dwh', f"""SELECT QUERY_ID, QUERY_TEXT FROM TABLE(UTIL_DB.information_schema.query_history(RESULT_LIMIT => 100)) 
+        WHERE EXECUTION_STATUS = 'RUNNING' AND USER_NAME NOT IN ({names})""")
     matcher = difflib.SequenceMatcher() #создаем экземпляр класса SequenceMatcher
     list4kill = []
-    for q in query_list:
-        matcher.set_seq2(q[1]) #устанавливаем первую последовательность для сравнения
-        for q2 in query_list:
-            if q[1] == q2[1]:
+    for query_text_1 in query_list:
+        matcher.set_seq2(query_text_1[1]) #устанавливаем первую последовательность для сравнения
+        for query_text_2 in query_list:
+            if query_text_1[1] == query_text_2[1]:
                 continue
-            matcher.set_seq1(q2[1]) #устанавливаем вторую последовательность для сравнения
+            matcher.set_seq1(query_text_2[1]) #устанавливаем вторую последовательность для сравнения
             if matcher.ratio() > match_percentage:
-                list4kill.append(q2[0])
+                similar_query_id = query_text_2[0]
+                list4kill.append(similar_query_id)
     return list4kill
 
 
-def max_time_running(connect):
-    max_ti_query_history = connect.query('dwh', """SELECT QUERY_ID FROM TABLE(UTIL_DB.information_schema.query_history(RESULT_LIMIT => 100)) 
-        WHERE EXECUTION_STATUS = 'RUNNING' AND USER_NAME NOT IN ('TEAMCITY', 'POWERBI', 'ETL', 'ETL_EMERGENCY') ORDER BY execution_time DESC LIMIT 1""")
+def get_longest_query(connect, names=usernames):
+    max_ti_query_history = connect.query('dwh', f"""SELECT QUERY_ID FROM TABLE(UTIL_DB.information_schema.query_history(RESULT_LIMIT => 100)) 
+        WHERE EXECUTION_STATUS = 'RUNNING' AND USER_NAME NOT IN ({names}) ORDER BY execution_time DESC LIMIT 1""")
     return max_ti_query_history[0][0]
 
 
-def if_the_queue(connect):
-    queue_query = connect.query('dwh', """SELECT COUNT(*) FROM TABLE(UTIL_DB.information_schema.query_history(RESULT_LIMIT => 100)) 
-            WHERE EXECUTION_STATUS = 'QUEUED' AND USER_NAME NOT IN ('TEAMCITY', 'POWERBI', 'ETL', 'ETL_EMERGENCY')""")
+def get_count_of_queries_in_queue(connect, names=usernames):
+    queue_query = connect.query('dwh', f"""SELECT COUNT(*) FROM TABLE(UTIL_DB.information_schema.query_history(RESULT_LIMIT => 100)) 
+            WHERE EXECUTION_STATUS = 'QUEUED' AND USER_NAME NOT IN ({names})""")
     return queue_query[0][0]
 
 
-def the_queue(connect):
+def kill_queries(connect):
     #находим похожие запросы и убиваем их
-    list = similar_queries(connect)
-    for query_id in list:
+    similar_queries_id = get_similar_queries(connect)
+    for query_id in similar_queries_id:
         connect.query('dwh', f"""SELECT system$cancel_query('{query_id}')""")
     #проверяем, если очередь
-    queue = if_the_queue(connect)
+    count_of_queries = get_count_of_queries_in_queue(connect)
     #если есть, то убиваем пока очередь не закончиться
-
-    while queue > 0:
-        longest_query = max_time_running(connect)
+    while count_of_queries > 0:
+        longest_query = get_longest_query(connect)
         connect.query('dwh', f"""SELECT system$cancel_query('{longest_query}')""")
-        queue = if_the_queue(connect)
+        count_of_queries = get_count_of_queries_in_queue(connect)
 
 
 if __name__ == '__main__':
-    the_queue(connect)
+    kill_queries()
